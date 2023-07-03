4b499b4e16c02a4abf58989f68de1f43f6e180e6
estii20 <>
2023-07-03 13:45:47 +0000

    Add function to get email data from the user anfd
    and update the guest feedback worksheet with guest email

--- run.py
+++ run.py
@@ -61,3 +61,73 @@ def update_name_guest_feedback_worksheet(name):
 
 name = get_name_data()
 update_name_guest_feedback_worksheet(name)
+
+
+def get_email_data():
+    """
+    Get guest email input from the user until
+    name data entered correctly.
+    """
+    while True:
+        print("Please enter guest email.\n")
+        print("Example: joe.blogs@gmail.com\n")
+
+        data_str = input("Enter guest email here: \n")
+
+        email_data = data_str.split("@")
+
+        if validate_email(email_data):
+            print("Email is valid!")
+            break
+
+    return email_data
+
+
+def validate_email(input):
+    """
+    Raises action from user if email not entered
+    in correct format.
+    """
+    try:
+        if input == validate_email:
+            print("Thanks\n")
+    except ValueError:
+        print("Email format incorrect, please try again\n")
+        return False
+
+    return True
+
+
+def update_email_guest_feedback_worksheet(email):
+    """
+    Update guest feedback worksheet,
+    add new row with the email data provided
+    """
+    print("Updating email of guest feedback worksheet...\n")
+    feedback_worksheet = SHEET.worksheet("feedback")
+    feedback_worksheet.append_row(email)
+    print("Email data updated successfully.\n")
+
+
+email = get_email_data()
+update_email_guest_feedback_worksheet(email)
+
+
+def get_front_desk_score():
+    """
+    Get guest name input from the user until
+    name data entered correctly.
+    """
+    while True:
+        print("Please enter guest name.\n")
+        print("Example: Joe Blogs\n")
+
+        data_str = input("Enter guest first name, space, last name here: \n")
+
+        name_data = data_str.split(" ")
+
+        if validate_name(name_data):
+            print("Name is valid!")
+            break
+
+    return name_data
